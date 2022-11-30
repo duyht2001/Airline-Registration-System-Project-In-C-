7c19505059077a8ec3e9a1d66753510b49cd59ee
duyht2001 <hoangthaiduy@hotmail.com>
2022-11-30 17:14:43 +0700

    update lan dau

---
+++ Airline Registration System.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+
+using namespace std;
+
+void mainMenu();
+
+
+class QuanLyCB
+{
+	public:
+	
+		QuanLyCB()
+		{
+			mainMenu();
+		}
+};
+
+void mainMenu()
+{
+	int luachon1;
+	int luachon2;
+	int quaylai;
+	
+	cout<<"\t   NTU Airlines \n"<<endl;
+	
+}
+
+int main()
+{
+	QuanLyCB();
+	return 0;
+}
