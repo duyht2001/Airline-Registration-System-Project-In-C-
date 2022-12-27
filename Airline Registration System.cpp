1412fe62feb8177175a5ee2777cba99585f58561
duyht2001 <hoangthaiduy@hotmail.com>
2022-12-27 07:51:36 +0700

    upadte bai con lai trong bai

--- Airline Registration System.cpp
+++ Airline Registration System.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void mainMenu();
 
 struct customer{
-	char ID[20];
+	string ID;
 	char name[20];
 	char add[50];
 	char email[50];
@@ -45,44 +45,166 @@ void add_kh(node &a, customer x)
     }
 }
 
-void xoa_kh(char id[], node &a)
+void xoa_kh(string id, node &a)
 {
     node tmp = a, pre = NULL;
     while(tmp != NULL)
     {
-        if(strcmp(tmp->data.ID, id) == 0)
+        if(tmp->data.ID == id)
         {
-            if(tmp = a)
+            if(tmp == a)
             {
                 a = tmp->next;
-                tmp->next = NULL;
+                free(tmp);
             }else
             {
                 pre->next = tmp->next;
-                tmp->next = NULL;
+                free(tmp);
             }
+            cout << "Da xoa ID: " << id << "thanh cong!\n";
+            return;
         }
         pre = tmp;
         tmp = tmp->next;
     }
+    cout << "Khong tim thay ID!\n";
 }
 
 void prin(node a)
 {
     ofstream file1("out.txt");
     node tmp = a;
+    cout << setw(15) << "ID" << setw(15) << "Ten" << setw(15) << "Tuoi" <<setw(15) << "Dia chi" <<setw(15) << "Noi den"<<setw(15) << "Noi khoi hanh" <<setw(15)<< "Gioi tinh"
+          <<setw(15) << "Ngay khoi hanh" << setw(15) << "Email" <<setw(30) << "So dien thoai"<< '\n';
     while(tmp != NULL) {
-        file1 << tmp->data.ID << '\t' << tmp->data.name << '\t' << tmp->data.age << '\t' << tmp->data.add << '\t' << tmp->data.arrival<< '\t' << tmp->data.departure << '\t'<< tmp->data.sex
-        << '\t' << tmp->data.date<< '\t' << tmp->data.email<< '\t' << tmp->data.mobile << '\n';
+        cout << setw(15) << tmp->data.ID << setw(15) << tmp->data.name << setw(15) << tmp->data.age <<setw(15) << tmp->data.add <<setw(15) << tmp->data.arrival<<setw(15) << tmp->data.departure <<setw(15)<< tmp->data.sex
+        <<setw(15) << tmp->data.date<<setw(15) << tmp->data.email<<setw(30) << tmp->data.mobile << '\n';
+        tmp = tmp -> next;
+    }
+    tmp = a;
+    while(tmp != NULL) {
+        file1 << tmp->data.ID << setw(15) << tmp->data.name << setw(15) << tmp->data.age <<setw(15) << tmp->data.add <<setw(15) << tmp->data.arrival<<setw(15) << tmp->data.departure <<setw(15)<< tmp->data.sex
+        <<setw(15) << tmp->data.date<<setw(15) << tmp->data.email<<setw(30) << tmp->data.mobile << '\n';
         tmp = tmp -> next;
     }
     file1 << endl;
     file1.close();
 }
 node cus_list = NULL;
+
+void chinh_sua_thong_tin(string id, node &a)
+{
+    char name[20];
+	char add[50];
+	char email[50];
+	long mobile;
+	int age;
+	char sex;
+	char departure[25];	//noi den
+	char arrival[25];
+	char date[10];
+
+	node tmp = a;
+	while(tmp != NULL)
+	{
+	    if(tmp->data.ID == id) break;
+	    tmp = tmp->next;
+	}
+    if(tmp == NULL) {
+        cout << "\t\t-----------------------------\n";
+        cout << "\t\t\tID khong co san!\n";
+        return;
+    }
+
+    int i = 1;
+    do
+    {
+        cout << "\n\tChon thong tin muon sua:\n";
+        cout << "\t\t-----------------------------\n";
+        cout << "\t1. Ten\n";
+        cout << "\t2. Dia chi\n";
+        cout << "\t3. email\n\t4. So dien thoai\n\t5. Tuoi\n\t6. Gioi tinh\n\t7. Noi den\n\t8. Noi di\n\t9. Ngay khoi hanh\n\t10. Thoat.\n";
+
+        int choose;
+        cin >> choose;
+        cin.get();
+        switch(choose)
+        {
+        case 1:
+        {
+            cout << "Nhap ten can sua:";
+            cin.getline(name, 20);
+            strcpy(tmp->data.name, name);
+            break;
+        }
+        case 2:
+        {
+            cout << "Nhap dia chi can sua:";
+            cin.getline(add, 50);;
+            strcpy(tmp->data.add, add);
+            break;
+        }
+        case 3:
+        {
+            cout << "Nhap email can sua:";
+            cin.getline(email, 50);
+            strcpy(tmp->data.email, email);
+            break;
+        }
+        case 4:
+        {
+            cout << "Nhap so dien thoat can sua:";
+            cin >> mobile;
+            tmp->data.mobile = mobile;
+            break;
+        }
+        case 5:
+        {
+            cout << "Nhap tuoi can sua:";
+            cin >> age;
+            tmp->data.age = age;
+            break;
+        }
+        case 6:
+        {
+            cout << "Nhap gioi tinh can sua:";
+            cin >> sex;
+            tmp->data.sex = sex;
+            break;
+        }
+        case 7:
+        {
+            cout << "Nhap noi den can sua:";
+            cin.getline(departure, 25);
+            strcpy(tmp->data.departure, departure);
+            break;
+        }
+        case 8:
+        {
+            cout << "Nhap noi di can sua:";
+            cin.getline(arrival, 25);
+            strcpy(tmp->data.arrival, arrival);
+            break;
+        }
+        case 9:
+        {
+            cout << "Nhap ngay khoi hanh can sua:";
+            cin.getline(date, 10);
+            strcpy(tmp->data.date, date);
+            break;
+        }
+        case 10:
+            --i;
+            break;
+        }
+    }
+    while(i);
+
+}
+
 class BOOKING
 {
-	public: char ID[20];
+	public: string ID;
 	char name[20];
 	char add[50];
 	char email[50];
@@ -208,7 +330,7 @@ class BOOKING
         strcpy(x.add, add);
         x.age = age;
         strcpy(x.arrival, arrival);
-        strcpy(x.ID , ID);
+        x.ID = ID;
         strcpy(x.date , date);
         strcpy(x.departure , departure);
         strcpy(x.email , email);
@@ -226,7 +348,7 @@ void Thongtin()
 void Xoa()
 {
     cout << "Nhap ID can xoa:";
-    char c[20]; cin >> c;
+    string c; cin >> c;
     xoa_kh(c, cus_list);
 }
 
@@ -235,57 +357,94 @@ void TimKiem();
 
 int main()
 {
-	cout << "\t\t\tWelcome Admin!\n";
-	start:
-		cout << "*****************************************************************\n";
-	cout << "............<<<Chao Mung Den Voi Dich Vu NTU Airline>>>........\n";
-	cout << "\n";
-	cout << "---111--------111-----1111111111111111111---------1111-------1111\n";
-	cout << "---111-11-----111-------------1111----------------1111-------1111\n";
-	cout << "---111--11----111-------------1111----------------1111-------1111\n";
-	cout << "---111---11---111-------------1111----------------1111-------1111\n";
-	cout << "---111----11--111-------------1111----------------1111-------1111\n";
-	cout << "---111----11--111-------------1111----------------1111-------1111\n";
-	cout << "---111-----11-111-------------1111----------------1111-------1111\n";
-	cout << "---111--------111-------------1111----------------111111111111111\n";
-	cout << "*****************************************************************\n";
-	int k = 0;
-	int choice;
-	do {
-		cout << "\t\tVui long lua chon lua chon cua ban\n";
-		cout << "--------------------------------------------------------------------\n";
-		cout << "\t1. Dang ki chuyen bay\t\t2. Thong tin chuyen bay\n";
-		cout << "\t3. Tim kiem chuyen bay\t\t4. Sua doi thong tin chuyen bay\n";
-		cout << "\t5. Huy bo chuyen bay\t\t6. Thong tin hanh khach\n";
-		cout << "\t\t\t7. Thoat chuong trinh.\n";
+    string userName;
+    string userPassword;
+    int loginAttempt = 0;
 
-		cout << "Vui long nhap lua chon cua ban: ";
-		cin >> choice;
-		if (choice < 1 || choice > 7)
-		{
-			cout << "\nLua chon sai. Vui long quy khach nhap lai: \n";
-			k++;
+    while (loginAttempt < 5)
+    {
+        cout << "user name: ";
+        cin >> userName;
+        cout << "user password: ";
+        char tmp;
+        while((tmp = getch()) != 13)
+        {
+        	userPassword += tmp;
+        	printf("*");
 		}
-	} while (k != 0);
+        cout <<endl;
+        if (userName == "admin" && userPassword == "admin")
+        {
+            cout << "\t\t\tWelcome Admin!\n";
+            start:
+            cout << "*****************************************************************\n";
+            cout << "............<<<Chao Mung Den Voi Dich Vu NTU Airline>>>........\n";
+            cout << "\n";
+            cout << "---111--------111-----1111111111111111111---------1111-------1111\n";
+            cout << "---111-11-----111-------------1111----------------1111-------1111\n";
+            cout << "---111--11----111-------------1111----------------1111-------1111\n";
+            cout << "---111---11---111-------------1111----------------1111-------1111\n";
+            cout << "---111----11--111-------------1111----------------1111-------1111\n";
+            cout << "---111----11--111-------------1111----------------1111-------1111\n";
+            cout << "---111-----11-111-------------1111----------------1111-------1111\n";
+            cout << "---111--------111-------------1111----------------111111111111111\n";
+            cout << "*****************************************************************\n";
+            int choice;
+            do
+            {
+                cout << "\t\tVui long lua chon lua chon cua ban\n";
+                cout << "--------------------------------------------------------------------\n";
+                cout << "\t1. Dang ki chuyen bay.\t\t2. Thong tin chuyen bay.\n";
+                cout << "\t3. Tim kiem chuyen bay.\t\t4. Sua doi thong tin chuyen bay.\n";
+                cout << "\t5. Huy bo chuyen bay.\t\t";
+                cout << "6. Thoat chuong trinh.\n";
 
-	switch (choice)
-	{
-		case 1:
-			DangKi();
-			break;
-		case 3:
-			TimKiem();
-			break;
-        case 2:
-            Thongtin();
-            break;
-        case 5:
-            Xoa();
-            break;
-		case 7:
-            exit(EXIT_FAILURE);
+                cout << "Vui long nhap lua chon cua ban: ";
+                cin >> choice;
+                if (choice < 1 || choice > 6)
+                {
+                    cout << "\nLua chon sai. Vui long quy khach nhap lai: \n";
+                    continue;
+                }
+                switch (choice)
+                {
+                case 1:
+                    DangKi();
+                    break;
+                case 2:
+                    Thongtin();
+                    break;
+                case 3:
+                    TimKiem();
+                    break;
 
-	}
+                case 4:
+                {
+                    cout << "\n\tNhap ID ban muon chinh sua:";
+                    string id_tam;
+                    cin >> id_tam;
+                    chinh_sua_thong_tin(id_tam, cus_list);
+                    break;
+                }
+                case 5:
+                {
+                    Xoa();
+                    break;
+                }
+                case 6:
+                {
+                    exit(EXIT_FAILURE);
+                }
+            }
+            }
+            while (true);
+        }
+        else
+        {
+            cout << "Dang nhap sai, vui long dang nhap lai.\n" << '\n';
+            loginAttempt++;
+        }
+    }
 }
 
 void DangKi()
@@ -327,7 +486,7 @@ void DangKi()
     cout << "\n\t..........Dat Chuyen Bay Thanh Cong...............\n";
 
     char c1;
-    cout << "\tNhan 'q' de ket thuc, nhan 'm' quay ve menu chinh, nhan 'd' de xoa ve";
+    cout << "\tNhan 'q' de ket thuc, nhan 'm' quay ve menu chinh.";
     cin >> c1;
     if (c1 == 'q' || c1 == 'Q')
         exit(EXIT_FAILURE);
@@ -335,196 +494,23 @@ void DangKi()
     {
         main();
     }
-    else if(c1 == 'D' || c1 == 'd')
-    {
-        cout << "Nhap ID can xoa:";
-        char tmp[20];
-        cin.getline(tmp, 20);
-        xoa_kh(tmp, cus_list);
-    }
 }
 
 void TimKiem()
 {
-	BOOKING s;
-	int choice;
-	fstream f3;
-	char sName[20];
-	char sEmail[20];
-	long sMobile;
-	char sID[20];
-	f3.open("booking.txt", ios:: in | ios::out | ios::binary);
-	cout << "*****************Chon tieu chi de Tim Kiem*******************\n";
-	cout << "---111--------111-----1111111111111111111-----1111-------1111\n";
-	cout << "---111-11-----111-------------1111------------1111-------1111\n";
-	cout << "---111--11----111-------------1111------------1111-------1111\n";
-	cout << "---111---11---111-------------1111------------1111-------1111\n";
-	cout << "---111----11--111-------------1111------------1111-------1111\n";
-	cout << "---111----11--111-------------1111------------1111-------1111\n";
-	cout << "---111-----11-111-------------1111------------1111-------1111\n";
-	cout << "---111--------111-------------1111------------111111111111111\n";
-	cout << "\t\t---------------------------------------------------------\n";
-	cout << "\n";
-	cout << "\t1. Theo Ten\n\t2. Theo Email\n";
-	cout << "\t3. Theo ID(Khuyen nghi)\n\t4. Theo SDT\n";
-
-	cout << "Vui long nhap lua chon cua ban: ";
-	cin >> choice;
-
-	switch (choice)
-	{
-		case 1:
-			{
-				int flag = 0;
-				cout << "Nhap ten ma ban muon tim kiem: ";
-				cin >> sName;
-				f3.seekg(0, ios::beg);
-				f3.read((char*) &s, sizeof(s));
-				cout.setf(ios::left);
-				cout << setw(15) << "ID" << setw(15) << "Ho va Ten" << setw(12) << "Ngay di" << setw(20) << "Dia chi" << setw(15) << "SDT" << setw(30) << "Email" << setw(12) << "Noi den" << setw(12) << "Noi di" << endl;
-				cout << setw(15) << "----" << setw(15) << "----" << setw(12) << "----" << setw(20) << "-------" << setw(15) << "------" << setw(30) << "-----" << setw(12) << "-------" << setw(12) << "-------" << endl;
-				while (f3.eof() != 1)
-				{
-					if (strcmp(sName, s.name) == 0)
-					{
-						s.disInfo();
-						flag = 1;
-					}
-
-					f3.read((char*) &s, sizeof(s));
-				}
-
-				if (flag == 0)
-					cout << "\t!!!!!!Khong tim thay ket qua!!!!!!!!\n";
-				f3.close();
-
-				char c3_1;
-				cout << "Nhan 'q' de quay lai Menu Tim Kiem\nNhan 'm' de quay lai Menu Chinh\n";
-				cin >> c3_1;
-
-				if (c3_1 == 'q' || c3_1 == 'Q')
-					TimKiem();
-				else if (c3_1 == 'm' || c3_1 == 'M')
-				{
-					main();
-				}
-			}
-
-			break;
-
-		case 2:
-			{
-				int flag = 0;
-				cout << "Nhap Email ma ban muon tim kiem:- ";
-				cin >> sEmail;
-
-				f3.seekg(0, ios::beg);
-				f3.read((char*) &s, sizeof(s));
-
-				cout.setf(ios::left);
-				cout << setw(15) << "ID" << setw(15) << "Ho va Ten" << setw(12) << "Ngay di" << setw(20) << "Dia chi" << setw(15) << "SDT" << setw(30) << "Email" << setw(12) << "Noi den" << setw(12) << "Noi di" << endl;
-				cout << setw(15) << "----" << setw(15) << "----" << setw(12) << "----" << setw(20) << "-------" << setw(15) << "------" << setw(30) << "-----" << setw(12) << "-------" << setw(12) << "-------" << endl;
-				while (f3.eof() != 1)
-				{
-					if (strcmp(sEmail, s.email) == 0)
-					{
-						s.disInfo();
-						flag = 1;
-					}
-
-					f3.read((char*) &s, sizeof(s));
-				}
-
-				if (flag == 0)
-					cout << "\t\tKhong tim thay ket qua!!\n";
-
-				char c3_2;
-				cout << "Nhan 'q' de quay lai Menu Tim Kiem nhan 'm' de quay lai Menu Chinh\n";
-				cin >> c3_2;
-
-				if (c3_2 == 'q' || c3_2 == 'Q')
-					TimKiem();
-				else if (c3_2 == 'm' || c3_2 == 'M')
-				{
-					main();
-				}
-			}
-
-			break;
-
-		case 3:
-			{
-				int flag = 0;
-				cout << "Nhap ID ma ban muon tim kiem: ";
-				cin >> sID;
-
-				f3.seekg(0, ios::beg);
-				f3.read((char*) &s, sizeof(s));
-
-				cout.setf(ios::left);
-				cout << setw(15) << "ID" << setw(15) << "Ho va Ten" << setw(12) << "Ngay di" << setw(20) << "Dia chi" << setw(15) << "SDT" << setw(30) << "Email" << setw(12) << "Noi den" << setw(12) << "Noi di" << endl;
-				cout << setw(15) << "----" << setw(15) << "----" << setw(12) << "----" << setw(20) << "-------" << setw(15) << "------" << setw(30) << "-----" << setw(12) << "-------" << setw(12) << "-------" << endl;
-
-				while (f3.eof() != 1)
-				{
-					if (sID == s.ID)
-					{
-						s.disInfo();
-						flag = 1;
-					}
-				}
-
-				if (flag == 0)
-					cout << "\t\tKhong co ket qua!!!!\n";
-
-				char c3_3;
-				cout << "Nhan 'q' de quay lai Menu Tim Kiem nhan 'm' de quay lai Menu Chinh\n";
-				cin >> c3_3;
-
-				if (c3_3 == 'q' || c3_3 == 'Q')
-					TimKiem();
-				else if (c3_3 == 'm' || c3_3 == 'M')
-				{
-					main();
-				}
-			}
-
-			break;
-
-		case 4:
-			{
-				int flag = 0;
-				cout << "Nhap SDT ma ban muon tim kiem: ";
-				cin >> sMobile;
-
-				f3.seekg(0, ios::beg);
-				f3.read((char*) &s, sizeof(s));
-
-				cout.setf(ios::left);
-				cout << setw(15) << "ID" << setw(15) << "Ho va Ten" << setw(12) << "Ngay di" << setw(20) << "Dia chi" << setw(15) << "SDT" << setw(30) << "Email" << setw(12) << "Noi den" << setw(12) << "Noi di" << endl;
-				cout << setw(15) << "----" << setw(15) << "----" << setw(12) << "----" << setw(20) << "-------" << setw(15) << "------" << setw(30) << "-----" << setw(12) << "-------" << setw(12) << "-------" << endl;
-				while (f3.eof() != 0)
-				{
-					if (sMobile, s.mobile)
-					{
-						s.disInfo();
-						flag = 1;
-					}
-				}
-
-				if (flag == 0)
-					cout << "\tKhong tim thay ket qua!!!!!!!!\n";
-
-				char c3_4;
-				cout << "Nhan 'q' de quay lai Menu Tim Kiem nhan 'm' de quay lai Menu Chinh\n";
-				cin >> c3_4;
-
-				if (c3_4 == 'q' || c3_4 == 'Q')
-					TimKiem();
-				else if (c3_4 == 'm' || c3_4 == 'M')
-				{
-					main();
-				}
-			}
-	}
+	cout << "Nhap ID can tim kiem:";
+	string tmp_id; cin >> tmp_id;
+	node tmp = cus_list;
+	while(tmp != NULL)
+    {
+        if(tmp->data.ID == tmp_id)
+        {
+            cout << "ID hop le!\n";
+            cout << setw(15) << tmp->data.ID << setw(15) << tmp->data.name << setw(15) << tmp->data.age <<setw(15) << tmp->data.add <<setw(15) << tmp->data.arrival<<setw(15) << tmp->data.departure <<setw(15)<< tmp->data.sex
+                 <<setw(15) << tmp->data.date<<setw(15) << tmp->data.email<<setw(30) << tmp->data.mobile << '\n';
+            return;
+        }
+        tmp = tmp->next;
+    }
+    cout << "ID khong tim thay!\n";
 }
